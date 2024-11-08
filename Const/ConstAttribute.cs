5ae55ce2d3f68bb4db03d4594011a09076add794
Qiu Shui <yeningyan_jin@roboticplus.com>
2024-11-08 09:36:49 +0800

    :memo: Add the comments for the Const project.

--- Const/ConstAttribute.cs
+++ Const/ConstAttribute.cs
@@ -1,7 +1,13 @@
 ﻿namespace Const;
 
+/// <summary>
+/// Make your method or parameter as const.
+/// </summary>
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Method)]
 public class ConstAttribute : Attribute
 {
+    /// <summary>
+    /// The way about how to const.
+    /// </summary>
     public ConstType Type { get; set; } = ConstType.All;
 }
--- Const/ConstType.cs
+++ Const/ConstType.cs
@@ -1,11 +1,34 @@
 ﻿namespace Const;
 
+/// <summary>
+/// The type of const.
+/// </summary>
 [Flags]
 public enum ConstType : byte
 {
+    /// <summary>
+    /// Nothing to const.
+    /// In this case, why do you use this <see cref="ConstAttribute"/>
+    /// </summary>
     None,
+    
+    /// <summary>
+    /// Can't modify the parameter itself.
+    /// </summary>
     Self = 1 << 0,
+    
+    /// <summary>
+    /// Can't modify the fields and properties of this parameter.
+    /// </summary>
     Members = 1 << 1,
+    
+    /// <summary>
+    /// Can't modify the fields and properties of the fields and properties of this parameter.
+    /// </summary>
     MembersInMembers = 1 << 2,
+    
+    /// <summary>
+    /// You can't modify anything.
+    /// </summary>
     All = Self | Members | MembersInMembers,
 }
