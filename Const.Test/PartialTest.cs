24c38b6e8859826ed20f914e97c66de0c9b55edb
秋水 <53346444+ArchiDog1998@users.noreply.github.com>
2024-11-08 23:51:49 +0800

    :white_check_mark: Add a partial test.

---
+++ Const.Test/PartialTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Const.Test.Type;
+
+public partial class PartialTest
+{
+    private partial void DoSomething([Const]string param1);
+}
+
+partial class PartialTest
+{
+    private partial void DoSomething(string param1)
+    {
+        param1 = "Nice";
+    }
+}
